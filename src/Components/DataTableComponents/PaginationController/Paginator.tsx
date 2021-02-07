e1bb499bd8d84db801fbfd53b4e5582891ab342a
David Simoneau <davidsimoneaujr@Gmail.com>
2021-02-06 20:42:23 -0700

    feat: adding paginator component

---
+++ src/Components/DataTableComponents/PaginationController/Paginator.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export function Paginator(
+    {
+        maxDataSize,
+        paginationIndex,
+        paginationSize,
+        setPaginationIndex
+    }: 
+    {
+        maxDataSize: number;
+        paginationIndex: number;
+        paginationSize: number;
+        setPaginationIndex: React.Dispatch<React.SetStateAction<number>>
+    }
+) {
+    function handlePreviousClick() {
+        setPaginationIndex(paginationIndex - paginationSize);
+    }
+
+    function handleNextClick() {
+        setPaginationIndex(paginationIndex + paginationSize);
+    }
+
+    const lastDataIndex = Math.min(paginationIndex+paginationSize, maxDataSize);
+
+    return (
+        <div>
+            <button
+                disabled={paginationIndex === 0}
+                onClick={handlePreviousClick}
+            >
+                Prev
+            </button>
+                { `${paginationIndex}-${lastDataIndex} out of ${maxDataSize}`}
+            <button
+                disabled={maxDataSize < paginationSize+paginationIndex}
+                onClick={handleNextClick}
+            >
+                Next
+            </button>
+        </div>
+    )
+}
